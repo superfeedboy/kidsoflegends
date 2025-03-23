ed9d7cadbf18d752818e56640b88f86cdc04e1e8
Gilles COGNIN <gillescognin@gmail.com>
2025-03-23 22:38:25 +0100

    Pushing initial Nuxt UI

---
+++ packages/front/.nuxt/nuxt-icon-server-bundle.mjs
@@ -0,0 +1,17 @@
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+function createRemoteCollection(fetchEndpoint) {
+  let _cache
+  return async () => {
+    if (_cache)
+      return _cache
+    const res = await fetch(fetchEndpoint).then(r => r.json())
+    _cache = res
+    return res
+  }
+}
+
+export const collections = {
+  'lucide': () => require('@iconify-json/lucide/icons.json'),
+  'simple-icons': () => require('@iconify-json/simple-icons/icons.json'),
+}
---
+++ packages/front/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-11-27'
+})
