9a8205c4b6b67bd90f2b0cc17b115de362ca3d5f
Gilles COGNIN <gillescognin@gmail.com>
2025-03-26 23:25:20 +0100

    feat: Preparing footer for social medias and responsivness

--- packages/front/app/components/Footer.vue
+++ packages/front/app/components/Footer.vue
@@ -1,11 +1,10 @@
 <template>
   <div
-    class="grid grid-flow-col grid-cols-12 w-full h-20 items-center justify-center inset-shadow-[0_15px_15px_rgba(0,0,0,.25)]"
+    class="grid grid-rows sm:grid-flow-col sm:grid-cols-12 w-full h-20 items-center justify-center inset-shadow-[0_15px_15px_rgba(0,0,0,.25)] p-4"
   >
-    <!-- <p class="text-center text-gray-500 text-sm">
-      &copy; 2025 KidsOfLegends. All rights reserved. 🚀 par Gilles COGNIN.
-    </p> -->
-    <p class="col-span-8 text-center text-gray-500 text-sm">
+    <p
+      class="order-3 sm:order-1 sm:col-span-8 text-center text-gray-500 text-sm"
+    >
       © 2025 Kids of Legends 🚀 par Gilles COGNIN. Kids of Legends is not
       endorsed by Riot Games and does not reflect the views or opinions of Riot
       Games or anyone officially involved in producing or managing League of
@@ -14,9 +13,30 @@
       Games, Inc.
     </p>
     <USeparator
-      orientation="vertical"
-      class="col-span-1 h-[60%] text-gray-500"
+      :orientation="orientation"
+      class="order-2 row-span-1 sm:col-span-1 h-[60%] text-gray-500 py-4 sm:py-0 sm:px-4"
     />
-    <p class="col-span-3 text-gray-500">Social medias</p>
+    <div
+      class="order-1 sm:order-3 sm:col-span-3 text-gray-500 flex justify-center"
+    >
+      <p>Social medias soon 👀</p>
+    </div>
   </div>
 </template>
+
+<script setup>
+const orientation = ref("vertical");
+
+onMounted(() => {
+  // Met à jour l'orientation en fonction de la largeur de la fenêtre
+  const updateOrientation = () => {
+    orientation.value = window.innerWidth < 650 ? "horizontal" : "vertical";
+  };
+
+  // Initialisation
+  updateOrientation();
+
+  // Ajoute un écouteur pour gérer les changements de taille de la fenêtre
+  window.addEventListener("resize", updateOrientation);
+});
+</script>
